f077efe2fab288b93e4d014bb8b186fabca73679
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-02-25 09:51:33 +0300

    remove

--- hello/hello.view.ts
+++ hello/hello.view.ts
@@ -1,12 +1,12 @@
 namespace $.$$ {
 
-	type TourItem = {
-		title: string;
-		content: string;
-		locator: string;
-	};
-	
-	const mockItems: TourItem[] = [
+	type $TourItem = {
+		title: string
+		content: string
+		locator: string
+	}
+
+	const mockItems: $TourItem[] = [
 		{
 			title: "1 заголовок",
 			content: "1 контент",
@@ -22,24 +22,16 @@ namespace $.$$ {
 			content: "3 контент",
 			locator: "[milis_hello_Text]",
 		},
-	];
+	]
 
 	export class $milis_hello extends $.$milis_hello {
-		constructor() {
-			super()
-			this.items(mockItems)
-		}
-
 		message() {
 			let name = this.name()
 			return name && `Hello, ${ name }!`
 		}
 
-		@ $mol_mem
-		items(items?: TourItem[]) {
-			return items ?? [];
-			// return mockItems ?? [];
+		items() {
+			return mockItems
 		}
-
 	}
 }
--- helper/helper.view.ts
+++ helper/helper.view.ts
@@ -1,79 +1,79 @@
+
+
 namespace $.$$ {
-	
-	type TourItem = {
-		title: string;
-		content: string;
-		locator: string;
-	};
-	
-	const mockItems: TourItem[] = [
-		{
-			title: "1 заголовок",
-			content: "1 контент",
-			locator: "[$milis_hello]",
-		},
-		{
-			title: "2 заголовок",
-			content: "2 контент",
-			locator: "[milis_hello_Button]",
-		},
-		{
-			title: "3 заголовок",
-			content: "3 контент",
-			locator: "[milis_hello_Text]",
-		},
-	];
+	type $TourItem = {
+		title: string
+		content: string
+		locator: string
+	}
+	// const mockItems: $TourItem[] = [
+	// 	{
+	// 		title: "1 заголовок",
+	// 		content: "1 контент",
+	// 		locator: "[$milis_hello]",
+	// 	},
+	// 	{
+	// 		title: "2 заголовок",
+	// 		content: "2 контент",
+	// 		locator: "[milis_hello_Button]",
+	// 	},
+	// 	{
+	// 		title: "3 заголовок",
+	// 		content: "3 контент",
+	// 		locator: "[milis_hello_Text]",
+	// 	},
+	// ]
 
 	export class $milis_helper extends $.$milis_helper {
 		@$mol_mem
-		show(value?: boolean): boolean {
-			return value ?? true;
+		show( value?: boolean ): boolean {
+			return value ?? true
 		}
 
 		@$mol_mem
-		items(value?: TourItem[]): TourItem[] {
-			return value ?? [];
+		items( value?: $TourItem[] ): $TourItem[] {
+			return value ?? []
 		}
 
 		@$mol_mem
-		currentStep(value?: number): number {
-			console.log("currentStep value", value);
-			return value ?? 0;
+		currentStep( value?: number ): number {
+			console.log( "currentStep value", value )
+			return value ?? 0
 		}
 
 		currentItem() {
-			return this.items()?.[this.currentStep()] ?? {};
+			return this.items()?.[ this.currentStep() ] ?? {}
 		}
 
 		currentItemTitle() {
-			return this.currentItem().title ?? "";
+			return this.currentItem().title ?? ""
 		}
 
 		currentItemContent() {
-			return this.currentItem().content ?? "";
+			return this.currentItem().content ?? ""
 		}
 
 		prevStep() {
-			if (this.currentStep() > 0) {
-				this.currentStep(this.currentStep() - 1);
+			if( this.currentStep() > 0 ) {
+				this.currentStep( this.currentStep() - 1 )
 			}
 		}
 		prevStepDisabled(): boolean {
-			return this.currentStep() === 0;
+			return this.currentStep() === 0
 		}
 
 		nextStep() {
-			if (this.currentStep() + 1 < this.items().length) {
-				this.currentStep(this.currentStep() + 1);
+			if( this.currentStep() + 1 < this.items().length ) {
+				this.currentStep( this.currentStep() + 1 )
 			}
 		}
 
 		nextStepDisabled(): boolean {
-			return this.currentStep() === this.items().length - 1;
+			return this.currentStep() === this.items().length - 1
 		}
 
 		endTour(): void {
-			this.show(false);
+			this.show( false )
 		}
 	}
 }
