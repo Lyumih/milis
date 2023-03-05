762eafc016f0bf6c871631a22bfedd61942f9e48
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-03-05 17:58:38 +0300

    video active

---
+++ skazka/hero/hero.view.ts
@@ -0,0 +1,10 @@
+namespace $ {
+	export type $milis_skazka_hero_type = {
+		hero: string
+		fairytale: string
+		description: string
+		audioUrl: string
+		videoUrl: string
+		textUrl: string
+	}
+}
