c780fc1c2030856e6f9cc0d78ee03de6e93d40a7
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-03-26 11:06:57 +0300

    fix inputs

--- vaop/vaop.view.ts
+++ vaop/vaop.view.ts
@@ -11,8 +11,11 @@ namespace $.$$ {
 	type AgentDTO = {
 		id: string
 		business?: string
+		machine?: string
+		programmer?: string
 		current_step?: number
 		next_step?: number
+		name?: string
 	}
 
 	export class $milis_vaop extends $.$milis_vaop {
@@ -20,10 +23,7 @@ namespace $.$$ {
 		agent_list( next?: AgentDTO[] ) {
 			console.log('FIX ME agent list', next)
 			return next ?? [
-				{ id: crypto.randomUUID(), business: 'test', current_step: 100, next_step: 200 },
-				{ id: crypto.randomUUID(), current_step:300, next_step: 400 },
-				// { id: crypto.randomUUID() },
-				// { id: 3 }
+				{ id: crypto.randomUUID(), business: '1 Бизнес требование', current_step: 100, next_step: 200 },
 			]
 		}
 
@@ -45,28 +45,57 @@ namespace $.$$ {
 		}
 
 		@$mol_mem_key
-		agent_machine( id: string, next?: string ) {
+		agent_business( id: string, next?: string ) {
+			const current = this.agent_list().find(agent => agent.id === id)
+			if (next === undefined) return current?.business ?? ''
+			const copy = {...current, id: current?.id!, business: next}
+			this.agent_list(this.agent_list().map(agent => agent === current ? copy : agent))
 			return next ?? ''
 		}
 
 		@$mol_mem_key
-		agent_business( id: string, next?: string ) {
-			console.log('FIX ME',id, next)
+		agent_machine( id: string, next?: string ) {
+			const current = this.agent_list().find(agent => agent.id === id)
+			if (next === undefined) return current?.machine ?? ''
+			const copy = {...current, id: current?.id!, machine: next}
+			this.agent_list(this.agent_list().map(agent => agent === current ? copy : agent))
 			return next ?? ''
 		}
+
 
 		@$mol_mem_key
 		agent_programmer( id: string, next?: string ) {
+			const current = this.agent_list().find(agent => agent.id === id)
+			if (next === undefined) return current?.programmer ?? ''
+			const copy = {...current, id: current?.id!, programmer: next}
+			this.agent_list(this.agent_list().map(agent => agent === current ? copy : agent))
+			return next ?? ''
+		}
+
+		@$mol_mem_key
+		agent_name( id: string, next?: string ) {
+			const current = this.agent_list().find(agent => agent.id === id)
+			if (next === undefined) return current?.name ?? ''
+			const copy = {...current, id: current?.id!, name: next}
+			this.agent_list(this.agent_list().map(agent => agent === current ? copy : agent))
 			return next ?? ''
 		}
 
 		@$mol_mem_key
 		current_step( id: string, next?: number ) {
+			const current = this.agent_list().find(agent => agent.id === id)
+			if (next === undefined) return current?.current_step ?? 0
+			const copy = {...current, id: current?.id!, current_step: next}
+			this.agent_list(this.agent_list().map(agent => agent === current ? copy : agent))
 			return next ?? 0
 		}
 
 		@$mol_mem_key
 		next_step( id: string, next?: number ) {
+			const current = this.agent_list().find(agent => agent.id === id)
+			if (next === undefined) return current?.next_step ?? 0
+			const copy = {...current, id: current?.id!, next_step: next}
+			this.agent_list(this.agent_list().map(agent => agent === current ? copy : agent))
 			return next ?? 0
 		}
 
