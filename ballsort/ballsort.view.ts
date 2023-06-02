aa8b67d5ea45efcdc523c6ff5ca3f613db23395e
Mikhail Ivanov <ivanovmp@msk.bcs.ru>
2023-06-02 17:33:01 +0300

    Push commit

--- ballsort/ballsort.view.ts
+++ ballsort/ballsort.view.ts
@@ -14,11 +14,12 @@ namespace $.$$ {
 
 		@ $mol_mem
 		columns() {
-			console.log(this.board().balls())
-			const result =  this.board().balls().map((balls, index) => this.Column(index))
-			console.log(result)
-			// return [this.Column3()]
-			return [...result]
+			return this.board().balls().map((balls, index) => this.Column(index))
+		}
+
+		@ $mol_mem_key
+		column_balls( id: any ): readonly any[] {
+			return this.board().balls()[id].map((ball, index)=> this.Ball(`${id}-${index}`))
 		}
 
 		@ $mol_mem
